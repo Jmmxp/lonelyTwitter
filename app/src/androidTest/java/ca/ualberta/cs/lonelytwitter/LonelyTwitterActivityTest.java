a9a783eb2a33b684287f95405969e7280d3873a8
Jmmxp <Jmmxp98@hotmail.com>
2018-10-25 18:29:31 -0600

    Add lab changes

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,21 +1,49 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
-import junit.framework.TestCase;
+import com.robotium.solo.Solo;
 
-/**
- * Created by wz on 14/09/15.
- */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+        super(LonelyTwitterActivity.class);
     }
 
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
 
+    @Override
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
     }
+
+    @Override
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
+
+    // all of the test methods should be independent of the other. Before running every test,
+    // setUp is automatically called and after finished running, tearDown is called
+
+    public void testTweet() {
+        // message is the message that prints when the test fails
+        solo.assertCurrentActivity("wrong activity", "LonelyTwitterActivity");
+    }
+
+    public void testEquals() {
+        assertEquals("not equal", "5", "5");
+    }
+
+    public void testAddTweet() {
+        EditText body = (EditText) solo.getView(R.id.body);
+
+        solo.enterText(body, "dont know");
+
+        solo.clickOnButton("Save");
+        solo.clearEditText(body);
+
+        assertTrue((solo.waitForText("dont know")));
+    }
+
 }
